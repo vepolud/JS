0a88d6292519e6523dcca8c163cde18955004872
vepolud <vepolud@gmail.com>
2022-06-19 12:46:26 +0300

    CRUD операции работают

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -73,7 +73,7 @@ public class UserService implements UserDetailsService {
             return false;
         }
 
-        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setRoles(Collections.singleton(new Role(2L, "USER")));
         userRepository.save(user);
         return true;
     }
